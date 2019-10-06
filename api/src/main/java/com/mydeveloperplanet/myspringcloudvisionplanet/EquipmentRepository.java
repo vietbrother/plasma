7f502b2bcc1cc52461a4e10544400adec6eac53c
vietbrother <anhmv.uet@gmail.com>
2019-10-07 00:42:08 +0700

    update api

--- api/src/main/java/com/mydeveloperplanet/myspringcloudvisionplanet/EquipmentRepository.java
+++ api/src/main/java/com/mydeveloperplanet/myspringcloudvisionplanet/EquipmentRepository.java
@@ -13,5 +13,5 @@ import org.springframework.data.repository.query.Param;
 public interface EquipmentRepository extends JpaRepository<Equipment, Long> {
 
     @Query(nativeQuery = true, value = "select * from p_equipment where code IN (:codes)")   // 3. Spring JPA In cause using native query
-    List<Equipment> findEquimentByListCode(@Param("names") List<String> codes);
+    List<Equipment> findEquimentByListCode(@Param("codes") List<String> codes);
 }
